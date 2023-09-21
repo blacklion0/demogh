5e6b3097318e788dd97193e301850041ec61bdc1
Divyesh Thakar <115477297+blacklion0@users.noreply.github.com>
2023-09-21 12:28:14 +0530

    Add files via upload

---
+++ prg2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    int a,b,c;
+    int avr; .,3                                                    2
+    cout<<"\n Enter three int number:";
+    cin>>a,b,c;
+
+    return 0;
+}
